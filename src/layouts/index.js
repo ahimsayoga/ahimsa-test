5f8f8cdcfac5ed196d795bc91a934c50c2202e5c
mccrodp <info@opensourceopenmind.ninja>
2018-03-19 12:36:09 +0900

    Added Rhythm from typography to styled components.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import styled from 'styled-components'
 import 'intl'
 
 import Header from '../components/Header'
-// import { rhythm } from '../utils/typography'
+import { rhythm } from '../utils/typography'
 import en from 'react-intl/locale-data/en'
 import 'intl/locale-data/jsonp/en'
 import ja from 'react-intl/locale-data/ja'
@@ -17,12 +17,11 @@ import 'intl/locale-data/jsonp/ja'
 const ContentWrapper = styled.section`
   margin: 0 auto;
   max-width: 650px;
+  margin-top: ${rhythm(1.5)};
+  margin-bottom: ${rhythm(1.5)};
+  padding-left: ${rhythm(3 / 4)};
+  padding-right: ${rhythm(3 / 4)};
 `
-//   margin-top: ${rhythm(1.5)};
-//   margin-bottom: ${rhythm(1.5)};
-//   padding-left: ${rhythm(3 / 4)};
-//   padding-right: ${rhythm(3 / 4)}
-// `
 
 // add concatenated locale data
 addLocaleData([...en, ...ja])
