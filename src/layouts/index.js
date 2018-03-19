7d606cbe0fb3ef2edc99fe52623bfc2bfe83d985
mccrodp <info@opensourceopenmind.ninja>
2018-03-19 12:33:29 +0900

    Add styled components to layout component.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,20 +3,21 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { getCurrentLangKey, getLangs, getUrlForLang } from 'ptz-i18n'
 import { IntlProvider, addLocaleData } from 'react-intl'
-// import styled from 'styled-components'
+import styled from 'styled-components'
 import 'intl'
 
 import Header from '../components/Header'
-import { rhythm } from '../utils/typography'
+// import { rhythm } from '../utils/typography'
 import en from 'react-intl/locale-data/en'
 import 'intl/locale-data/jsonp/en'
 import ja from 'react-intl/locale-data/ja'
 import 'intl/locale-data/jsonp/ja'
 
 // Create a Wrapper component that'll render a <section> tag with some styles
-// const ContentWrapper = styled.section`
-//   margin: 0 auto;
-//   max-width: 650px;
+const ContentWrapper = styled.section`
+  margin: 0 auto;
+  max-width: 650px;
+`
 //   margin-top: ${rhythm(1.5)};
 //   margin-bottom: ${rhythm(1.5)};
 //   padding-left: ${rhythm(3 / 4)};
@@ -52,11 +53,9 @@ const TemplateWrapper = ({ children, data, location }) => {
           ]}
         />
         <Header langs={langsMenu} locale={langKey} />
-        {/* <ContentWrapper> */}
-        <div>
+        <ContentWrapper>
           {children()}
-        </div>
-        {/* </ContentWrapper> */}
+        </ContentWrapper>
       </div>
     </IntlProvider>
   )
